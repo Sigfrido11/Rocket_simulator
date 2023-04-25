df83475fbdb87b64c4db15d6407d60f480633cd0
giuseppe <giuseppe.luciano@studio.unibo.it>
2023-04-25 19:03:47 +0200

    alcune modifiche e implementazione tempo e stage relase

--- main.cpp
+++ main.cpp
@@ -1,35 +1,28 @@
 #include "menu.h"
-#include 'navigate.h'
-#include "simulation.h"
+#include "navigate.h"
 #include <SFML/Window.hpp>
-
-
-  menu::Setting_menu setting;
-         // check all the window's events that were triggered since the last iteration of the loop
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-
-            // "close requested" event: we close the window
-            if (event.type == sf::Event::Closed)
-                window.close();
-      
-
+#include <chrono>
+#include "array"
+#include "rocket.h"
+#include "simulation.h"
 
 int main() {
   // definisco le varie parti delle impostazioni
-  simulation::Rocket rocket;
+  rocket::Rocket rocket;
   simulation::Atmosphere atm;
+  simulation::vec force;
+  simulation::Gravity gravity;
+  auto start = chrono::high_resolution_clock;
+  auto end = chrono::high_resolution_clock;;
+  std::chrono::duration<double,std::nano> time; // è un double di nanosecondi
+                                                // se va a intaccare le performance mettiamo micro sec
   interface::Main_menu main_menu;
   interface::Example_menu preset;
   interface::Setting_menu setting;
   interface::Menu menu;
 
+  char menu_pos{'main'};  // ATTENZIONE IN REALTÀ È UNA STRINGA/INTERO
 
-
-  char menu_pos{'main'}; // ATTENZIONE IN REALTÀ È UNA STRINGA/INTERO
-
-  
   sf::Window window(sf::VideoMode(800, 600), "My window");
 
   // run the program as long as the window is open
@@ -38,12 +31,27 @@ int main() {
     // iteration of the loop
     sf::Event event;
 
-    if (menu_pos=='play'){
-      //codice di simulazione
+    if (menu_pos == 'play') {
+      // game loop
+      bool keep{true};
+      while (keep) {
+        auto start = chrono::high_resolution_clock::now();
+        force = simulation::force(rocket, atm, gravity);
+        rocket.mass_lose();
+        simulate::acelerate(rocket, force, time);
+        rocket.move(time);
+        rocket.move_theta();
+       // stage relese, verifica fatta nella spinta
+
+
+
+        end = chrono::high_resolution_clock();
+        time= start-end;
+         
+      }
+    } else 
+      navigation::Navigate(menu_pos, rocket);
     }
-    else{
-   navigation::Navigate(menu_pos, rocket);
-    }
-  
-  
-}}
+  }
+}
+// tempo chrono o clock?
--- menu.h
+++ menu.h
@@ -1,6 +1,7 @@
 #ifndef MENU_H
 #define MENU_H
 
+
 #include <SFML/Linux>
 #include "simulation.h"
 
