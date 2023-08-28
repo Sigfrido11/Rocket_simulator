89d2aef3cb71e870c04890a30e9877f00c306164
Stefano Doria <doria.stefano.0@gmail.com>
2023-08-28 17:08:49 +0200

    various work

--- tot1.cpp
+++ tot1.cpp
@@ -4,27 +4,32 @@
 #include <string>
 #include "rocket.h"
 #include "simulation.h"
-//#include "menu.h"
-//#include "navigate.h"
+// #include "menu.h"
+// #include "navigate.h"
 #include <stdio.h>
 #include <fstream>
 #include <iostream>
 
-void run_countdown(sf::Text& countdown, std::vector<sf::Drawable*>& drawables,
-                   std::vector<sf::Vertex*>& vertices,
-                   sf::RenderWindow& window) {
+void run_countdown(sf::Text &countdown, std::vector<sf::Drawable *> &drawables,
+                   std::vector<sf::Vertex *> &vertices,
+                   sf::RenderWindow &window)
+{
   sf::Clock clock;
-  for (int i = 10; i >= 1; --i) {
+  for (int i = 10; i >= 1; --i)
+  {
     countdown.setString(std::to_string(i));
     window.clear(sf::Color::White);
     window.display();
-    sf::Time dur=sf::Time::Zero;
+    sf::Time dur = sf::Time::Zero;
     clock.restart();
-    while (dur.asSeconds() < 1.0f) {
+    while (dur.asSeconds() < 1.0f)
+    {
       std::for_each(drawables.begin(), drawables.end(),
-                    [&](sf::Drawable* obj) { window.draw(*obj); });
+                    [&](sf::Drawable *obj)
+                    { window.draw(*obj); });
       std::for_each(vertices.begin(), vertices.end(),
-                    [&](sf::Vertex* obj) { window.draw(obj, 2, sf::Lines); });
+                    [&](sf::Vertex *obj)
+                    { window.draw(obj, 2, sf::Lines); });
       window.draw(countdown);
       dur = clock.getElapsedTime();
     }
@@ -33,15 +38,25 @@ void run_countdown(sf::Text& countdown, std::vector<sf::Drawable*>& drawables,
   countdown.setString("LIFT OFF!");
   window.clear(sf::Color::White);
   std::for_each(drawables.begin(), drawables.end(),
-                [&](sf::Drawable* obj) { window.draw(*obj); });
+                [&](sf::Drawable *obj)
+                { window.draw(*obj); });
 
   std::for_each(vertices.begin(), vertices.end(),
-                [&](sf::Vertex* obj) { window.draw(obj, 2, sf::Lines); });
+                [&](sf::Vertex *obj)
+                { window.draw(obj, 2, sf::Lines); });
   window.draw(countdown);
   window.display();
 }
 
-int main() {
+void set_text_style(sf::Text &text, sf::Font &font)
+{
+  text.setFont(font);
+  text.setCharacterSize(24);
+  text.setFillColor(sf::Color::White);
+}
+
+int main()
+{
   float width{1200.f};
   float height{600.f};
   sf::RenderWindow window(sf::VideoMode(width, height), "Rocket simulator");
@@ -51,12 +66,14 @@ int main() {
   window.setFramerateLimit(60);
 
   sf::Music music;
-  if (!music.openFromFile("01_A Better Beginning.wav")) return -1;  // error
+  if (!music.openFromFile("01_A Better Beginning.wav"))
+    return -1; // error
   music.play();
   music.setLoop(true);
 
   sf::Texture texture1;
-  if (!texture1.loadFromFile("rocket.png")) {
+  if (!texture1.loadFromFile("rocket.png"))
+  {
     // error
   }
   sf::Sprite rocket1;
@@ -78,7 +95,8 @@ int main() {
   ground.setPosition(0.f, height / 4.f * 3.f);
 
   sf::Texture texture2;
-  if (!texture2.loadFromFile("earth.jpeg")) {
+  if (!texture2.loadFromFile("earth.jpeg"))
+  {
     // error
   }
   sf::Sprite earth;
@@ -91,7 +109,8 @@ int main() {
   rocket2.setOrigin(1.f, 1.f);
 
   sf::Texture texture3;
-  if (!texture3.loadFromFile("map.jpg")) {
+  if (!texture3.loadFromFile("map.jpg"))
+  {
     // error
   }
   sf::Sprite map;
@@ -104,56 +123,45 @@ int main() {
   rocket3.setOrigin(2.f, 2.f);
   rocket3.setPosition(750.f, height / 4 * 3);
 
-  sf::Font font;
-  if (!font.loadFromFile("times_new_roman.ttf")) {
+  sf::Font tnr;
+  if (!tnr.loadFromFile("times_new_roman.ttf"))
+  {
     // error
   }
 
   sf::Text altitude;
-  altitude.setFont(font);
+  set_text_style(altitude, tnr);
   altitude.setString("Altitude: ");
-  altitude.setCharacterSize(24);
-  altitude.setFillColor(sf::Color::White);
   altitude.setPosition((width - 500.f) / 2 + 500.f + 50.f, 30.f);
 
   sf::Text angle;
-  angle.setFont(font);
+  set_text_style(angle, tnr);
   angle.setString("Angle: ");
-  angle.setCharacterSize(24);
-  angle.setFillColor(sf::Color::White);
   angle.setPosition((width - 500.f) / 2 + 500.f + 50.f, 70.f);
 
   sf::Text speed;
-  speed.setFont(font);
+  set_text_style(speed, tnr);
   speed.setString("Speed: ");
-  speed.setCharacterSize(24);
-  speed.setFillColor(sf::Color::White);
   speed.setPosition((width - 500.f) / 2 + 500.f + 50.f, 110.f);
 
   sf::Text stage;
-  stage.setFont(font);
+  set_text_style(stage, tnr);
   stage.setString("Stage: ");
-  stage.setCharacterSize(24);
-  stage.setFillColor(sf::Color::White);
   stage.setPosition((width - 500.f) / 2 + 500.f + 50.f, 150.f);
 
   sf::Text fuel_left;
-  fuel_left.setFont(font);
+  set_text_style(fuel_left, tnr);
   fuel_left.setString("Fuel left: ");
-  fuel_left.setCharacterSize(24);
-  fuel_left.setFillColor(sf::Color::White);
   fuel_left.setPosition((width - 500.f) / 2 + 500.f + 50.f, 190.f);
 
   int out_time{};
 
   sf::Text time;
-  time.setFont(font);
-  time.setCharacterSize(24);
-  time.setFillColor(sf::Color::White);
+  set_text_style(time, tnr);
   time.setPosition((width - 500.f) / 2 + 500.f + 50.f, 230.f);
 
   sf::Text countdown;
-  countdown.setFont(font);
+  countdown.setFont(tnr);
   countdown.setCharacterSize(400);
   countdown.setFillColor(sf::Color::Yellow);
   countdown.setPosition((width - 500.f) / 2, 20.f);
@@ -168,11 +176,11 @@ int main() {
       sf::Vertex(sf::Vector2f((width - 500.f) / 2 + 500.f, 0.f)),
       sf::Vertex(sf::Vector2f((width - 500.f) / 2 + 500.f, height / 2))};
 
-  std::vector<sf::Drawable*> drawables{
-      &map,      &ground, &inner_atm, &rocket1, &earth,     &rocket2, &rocket3,
-      &altitude, &angle,  &speed,     &stage,   &fuel_left, &time};
+  std::vector<sf::Drawable *> drawables{
+      &map, &ground, &inner_atm, &rocket1, &earth, &rocket2, &rocket3,
+      &altitude, &angle, &speed, &stage, &fuel_left, &time};
 
-  std::vector<sf::Vertex*> vertices{line, line1, line2};
+  std::vector<sf::Vertex *> vertices{line, line1, line2};
 
   // inizializzazione parametri
 
@@ -204,11 +212,11 @@ int main() {
   vec_liq.push_back(std::move(ad_1));
   vec_liq.push_back(std::move(ad_2));
   int n_solid_eng{1};
-  std::vector<int> n_liq_eng{1};
+  std::vector<int> n_liq_eng{1, 1};
 
-  rocket::Rocket rocket{name,  mass_structure, Up_Ar,       Lat_Ar,
-                        s_p_m, m_s_cont,       l_p_m,       l_c_m,
-                        eng_b, vec_liq,        n_solid_eng, n_liq_eng};
+  rocket::Rocket rocket{name, mass_structure, Up_Ar, Lat_Ar,
+                        s_p_m, m_s_cont, l_p_m, l_c_m,
+                        eng_b, vec_liq, n_solid_eng, n_liq_eng};
 
   sim::Air_var air;
   int i{0};
@@ -225,7 +233,8 @@ int main() {
 
   // game loop starts
 
-  while (window.isOpen()) {
+  while (window.isOpen())
+  {
     sf::Event event;
 
     bool orbiting =
@@ -253,9 +262,12 @@ int main() {
     output_air << air.t_ << " " << air.p_ << " " << air.rho_ << '\n';
     i += 1;
 
-    while (window.pollEvent(event)) {
-      if (event.type == sf::Event::Closed) window.close();
-      if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
+    while (window.pollEvent(event))
+    {
+      if (event.type == sf::Event::Closed)
+        window.close();
+      if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+      {
         window.close();
       }
     }
@@ -280,17 +292,20 @@ int main() {
 
     rocket3.move(0.1f, 0.f);
     const sf::Vector2f a{rocket3.getPosition()};
-    if (a.x > width) {
+    if (a.x > width)
+    {
       rocket3.setPosition(a.x - (width - 500.f), a.y);
     }
 
     window.clear();
 
     std::for_each(drawables.begin(), drawables.end(),
-                  [&](sf::Drawable* obj) { window.draw(*obj); });
+                  [&](sf::Drawable *obj)
+                  { window.draw(*obj); });
 
     std::for_each(vertices.begin(), vertices.end(),
-                  [&](sf::Vertex* obj) { window.draw(obj, 2, sf::Lines); });
+                  [&](sf::Vertex *obj)
+                  { window.draw(obj, 2, sf::Lines); });
 
     window.display();
 
